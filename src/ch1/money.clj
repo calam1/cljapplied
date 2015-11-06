c210930c498b8cc50add98cb1a7839a4c95b2175
Christopher Lam <chrislam67@gmail.com>
2015-11-05 22:01:41 -0600

    add multimethod example

--- src/ch1/money.clj
+++ src/ch1/money.clj
@@ -40,3 +40,5 @@
   ([] (make-money 0))
   ([amount] (make-money amount :usd))
   ([amount currency] (->Money amount (get currencies currency))))
+
+(def zero-dollars (make-money))
---
+++ src/ch1/multimethods.clj
@@ -0,0 +1,29 @@
+(ns ch1.multimethods
+  (:require [schema.core :as s]
+            [ch1.recipe]
+            [ch1.recipe :refer (->Person)]
+            [ch1.recipe :refer (->Ingredient)]
+            [ch1.money :refer [+$ zero-dollars]])
+  (:import [ch1.recipe Recipe]))
+
+(s/defrecord Store
+    [name :- s/Str
+     manager :- Person])
+
+(s/defn cost-of 
+  [store :- Store ingredient :- Ingredient]
+  (str (get store :name) (get ingredient :quantity)))
+
+(def test-store
+  (->Store
+   "Marshall Fields"
+           (->Person "Chris" "Lam")))
+
+(def test-ingredient
+  (->Ingredient "spaghetti" 1 :lb))
+
+(defmulti cost (fn [entity store] (class entity)))
+
+(defmethod cost Recipe [recipe store]
+  (reduce +$ zero-dollars
+          (map #(cost % store) (:ingredients recipe))))
