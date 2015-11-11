a0b4b4edc6bb0a69c88383c1fa3d61a2b7b65ccd
Chris Lam <chris.lam@oracle.com>
2015-11-11 09:09:16 -0600

    update.clj

--- src/ch2/update.clj
+++ src/ch2/update.clj
@@ -1,16 +1,20 @@
 (ns ch2.update
-  (require [medley.core :refer (map-keys)]
-           [ch1.recipe :refer (->Recipe)]))
+  (require [medley.core :refer (map-vals)]
+           [ch1.recipe :refer [->Ingredient]]
+           [ch1.recipe :refer (map->Recipe)]))
 
 (defn keywordize-entity
        [entity]
        (map-keys keyword entity))
 
-(defn compute-calories)
+(defn compute-calories
+  [entity]
+  300)
 
 (defn- update-calories
   [recipe]
-  (assoc recipe :calories (compute-calories recipe)))a
+  (assoc recipe :calories (compute-calories recipe)))
+
 
 (defn include-calories
   [recipe-index]
